d495280b5dbe3e57cb19f2a4d8e40271869280a1
DMD <daodu.muyiwa@gmail.com>
2020-05-13 17:48:40 -0600

    Updates

---
+++ precache-manifest.78083f5a1778f4c5a9e4cfb03d2fb04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2108e8469fe0edf5390b3ce7fe25ce84",
+    "url": "/atmCard/index.html"
+  },
+  {
+    "revision": "688ec3265b01f435d736",
+    "url": "/atmCard/static/css/main.2fc36c9b.chunk.css"
+  },
+  {
+    "revision": "87f27d2ecf92509876b3",
+    "url": "/atmCard/static/js/2.697cfb5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/atmCard/static/js/2.697cfb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688ec3265b01f435d736",
+    "url": "/atmCard/static/js/main.bfa56bbd.chunk.js"
+  },
+  {
+    "revision": "f49b89bd318e8af53bc0",
+    "url": "/atmCard/static/js/runtime-main.9117e675.js"
+  }
+]);
